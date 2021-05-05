d257dfe396c867da7884d9de9d81147f151f455a
Jake <jakehernandez397@gmail.com>
2021-05-05 16:47:34 +0100

    immlement basic login

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -59,7 +59,7 @@ foo@bar_baz.com foo@bar+baz.com]
     assert_not @user.valid?
   end
 
-  test "passoerd should have a minimum length" do
+  test "password should have a minimum length" do
     @user.password = @user.password_confirmation = "a" * 5
     assert_not @user.valid?
   end
